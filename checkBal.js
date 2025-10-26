f5c14178f8579faa896fc77f226f4700bddd8358
Sultan2403 <sultanremilekun@gmail.com>
2025-10-26 14:53:42 +0100

    balance checking up and running

--- checkBal.js
+++ checkBal.js
@@ -1,2 +1,8 @@
 import { customers, bankName } from "./app.js";
-const accID = parseInt(document.getElementById("acc-id").value);
+const balanceBtn = document.getElementById("checkBalBtn");
+const AccID = document.getElementById("acc-id");
+function chkBal(accID) {
+  const foundAcc = customers.find((found) => found.accId === accID);
+  console.log(foundAcc);
+}
+balanceBtn.addEventListener("click", () => chkBal(parseInt(AccID.value)));
